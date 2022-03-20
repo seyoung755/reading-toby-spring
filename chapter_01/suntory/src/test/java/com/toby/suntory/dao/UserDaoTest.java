362e78368a4756a16f9865ae11b6c60437e5d4fc
seyeong <seyoung7555@naver.com>
2022-03-21 02:16:27 +0900

    feat: count 단위테스트 추가

--- chapter_01/suntory/src/test/java/com/toby/suntory/dao/UserDaoTest.java
+++ chapter_01/suntory/src/test/java/com/toby/suntory/dao/UserDaoTest.java
@@ -3,14 +3,13 @@ package com.toby.suntory.dao;
 import com.toby.suntory.user.dao.DaoFactory;
 import com.toby.suntory.user.dao.UserDao;
 import com.toby.suntory.user.domain.User;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class UserDaoTest {
 
@@ -34,5 +33,27 @@ class UserDaoTest {
 
         assertThat(user2.getName()).isEqualTo(user.getName());
         assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    @Test
+    void count() throws SQLException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("user1", "유저1", "pw1");
+        User user2 = new User("user2", "유저2", "pw2");
+        User user3 = new User("user3", "유저3", "pw3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isZero();
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
     }
 }
