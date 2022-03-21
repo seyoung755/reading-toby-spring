0d9f7e0bfe03aaf21ce166aa59117e8f26c27dea
seyeong <seyoung7555@naver.com>
2022-03-22 00:40:01 +0900

    feat: UserDao의 get 테스트 발전시키기

--- chapter_01/suntory/src/test/java/com/toby/suntory/dao/UserDaoTest.java
+++ chapter_01/suntory/src/test/java/com/toby/suntory/dao/UserDaoTest.java
@@ -6,10 +6,12 @@ import com.toby.suntory.user.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserDaoTest {
 
@@ -18,21 +20,20 @@ class UserDaoTest {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean("userDao", UserDao.class);
-        User user = new User();
-        user.setId("suntory");
-        user.setName("산토리");
-        user.setPassword("springno1");
+        User user1 = new User("suntory", "산토리", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
 
         dao.deleteAll();
         assertThat(dao.getCount()).isZero();
 
-        dao.add(user);
-        assertThat(dao.getCount()).isEqualTo(1);
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
 
-        User user2 = dao.get(user.getId());
+        User userget2 = dao.get(user2.getId());
 
-        assertThat(user2.getName()).isEqualTo(user.getName());
-        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+        assertThat(user2.getName()).isEqualTo(user2.getName());
+        assertThat(user2.getPassword()).isEqualTo(user2.getPassword());
     }
 
     @Test
@@ -55,5 +56,17 @@ class UserDaoTest {
 
         dao.add(user3);
         assertThat(dao.getCount()).isEqualTo(3);
+    }
+
+    @Test
+    void getUserFailure() throws SQLException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+
+        assertThat(dao.getCount()).isZero();
+        assertThrows(EmptyResultDataAccessException.class, () -> dao.get("unknown_id"));
     }
 }
