dc92c210e2138726a77495981f55932caaa4be95
seyeong <seyoung7555@naver.com>
2022-03-31 00:39:28 +0900

    initial commit

---
+++ chapter_02/suntory/src/test/java/com/example/suntory/SuntoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suntory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuntoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
