749de9a20ab632f9f149164634beb68e5b66a971
seyeong <seyoung7555@naver.com>
2022-04-04 22:29:51 +0900

    feat: 토비의 스프링 3단계 시작

---
+++ chapter_03/suntory/src/main/java/com/toby/suntory/user/dao/UserDao.java
@@ -0,0 +1,89 @@
+package com.toby.suntory.user.dao;
+
+import com.toby.suntory.user.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    private DataSource dataSource;
+
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into user(id, name, password) values (?, ?, ?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from user where id = ?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+        rs.close();
+        ps.close();
+        c.close();
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        return user;
+    }
+
+    public void deleteAll() throws SQLException {
+
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("DELETE FROM user");
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM user");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
+}
---
+++ chapter_03/suntory/src/test/java/com/toby/suntory/dao/UserDaoTest.java
@@ -0,0 +1,79 @@
+package com.toby.suntory.dao;
+
+import com.toby.suntory.user.dao.DaoFactory;
+import com.toby.suntory.user.dao.UserDao;
+import com.toby.suntory.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DaoFactory.class})
+@DirtiesContext // 테스트 코드에서 애플리케이션의 컨텍스트를 수정하겠다는 표시
+class UserDaoTest {
+    // 스프링이 context를 만들어 주입한다. 각 test 오브젝트가 동일한 context를 사용한다.
+    @Autowired
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void setUp() {
+        user1 = new User("user1", "유저1", "pw1");
+        user2 = new User("user2", "유저2", "pw2");
+        user3 = new User("user3", "유저3", "pw3");
+        DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/toby", "root", "950104elql!", true);
+        dao.setDataSource(dataSource);
+    }
+
+    @Test
+    void addAndGet() throws SQLException {
+        dao.deleteAll();
+        assertThat(dao.getCount()).isZero();
+
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        User userget2 = dao.get(user2.getId());
+
+        assertThat(user2.getName()).isEqualTo(user2.getName());
+        assertThat(user2.getPassword()).isEqualTo(user2.getPassword());
+    }
+
+    @Test
+    void count() throws SQLException {
+        dao.deleteAll();
+        assertThat(dao.getCount()).isZero();
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
+    }
+
+    @Test
+    void getUserFailure() throws SQLException {
+        dao.deleteAll();
+
+        assertThat(dao.getCount()).isZero();
+        assertThrows(EmptyResultDataAccessException.class, () -> dao.get("unknown_id"));
+    }
+}
