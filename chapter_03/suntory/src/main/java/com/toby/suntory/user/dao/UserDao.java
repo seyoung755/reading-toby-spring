380f4480c0a114e702c01742c7a6a8c150d53533
seyeong <seyoung7555@naver.com>
2022-04-04 22:49:08 +0900

    refactor: 로컬 클래스를 통해 세부 전략 생성

--- chapter_03/suntory/src/main/java/com/toby/suntory/user/dao/UserDao.java
+++ chapter_03/suntory/src/main/java/com/toby/suntory/user/dao/UserDao.java
@@ -21,6 +21,24 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
+        class AddStatement implements StatementStrategy {
+            User user;
+
+            public AddStatement(User user) {
+                this.user = user;
+            }
+
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into user(id, name, password) values (?,?,?)");
+
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        }
         StatementStrategy strategy = new AddStatement(user);
         jdbcContextWithStatementStrategy(strategy);
     }
