ec674ddb6b0e817401a9656bb10561bccd36dc59
seyeong <seyoung7555@naver.com>
2022-04-07 01:58:33 +0900

    feat: 중복 사용되는 RowMapper 분리하기

--- chapter_03/suntory/src/main/java/com/toby/suntory/user/dao/UserDao.java
+++ chapter_03/suntory/src/main/java/com/toby/suntory/user/dao/UserDao.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
